b7aca578e322d815955743a5f354bbc46ecb31bf
Alec Dye <alecdye@gmail.com>
2020-02-13 14:01:50 -0800

    passing input render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
+// Pass
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+// Pass
+test('firstName, lastName, email inputs are rendered', () => {
+  const { getByLabelText } = render(<ContactForm />);
+  getByLabelText(/first name/i);
+  getByLabelText(/last name/i);
+  getByLabelText(/email/i);
+})
